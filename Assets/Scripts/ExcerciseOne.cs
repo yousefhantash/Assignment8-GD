3e72836ce11dade74286d50ef8fcdc16bd872abb
yousefhantash <yousef.pizza17@gmail.com>
2024-10-29 07:12:59 +0200

    first commit

--- Assets/Scripts/ExcerciseOne.cs
+++ Assets/Scripts/ExcerciseOne.cs
@@ -7,8 +7,21 @@ public class ExcerciseOne : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        while (true)
+        {
+            int number = Random.Range(1, 21);
+            if (number == 5)
+            {
+                continue;
+            }
+            if (number == 15)
+            {
+                break;
+            }
+            Debug.Log(number);
+        }
     }
+
 
     // Update is called once per frame
     void Update()
--- Assets/Scripts/ExcerciseTwo.cs
+++ Assets/Scripts/ExcerciseTwo.cs
@@ -7,12 +7,22 @@ public class ExcerciseTwo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        string[] words = { "Cat", "Dog", "Car", "Pizza", "Hat", "Fish", "Tree", "Monkey", "Ball", "Bird" };
+        string sentence = "";
+
+
+        while (true)
+        {
+            int index = Random.Range(0, words.Length);
+            sentence += words[index] + " ";
+
+        }
+        Debug.Log("Funny Sentence: " + sentence);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
